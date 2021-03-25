79d86e3635abba66cdc3543c3608dc6d5e686389
Han Wang <hyw2@cornell.edu>
2021-03-25 17:09:21 -0400

    Add email configuration

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const app = express();
 app.use(bodyParser.json());
 
 app.post('/', async (req, res) => {
-  const { email } = req.body;
+  const { email, examples, visuals, logical, storytelling } = req.body;
+
+  const msg = {
+    to: email, // Change to your recipient
+    from: 'hyw2@cornell.edu',
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log('Email sent');
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
   res.send(email);
 });
 
