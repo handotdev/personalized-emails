a9a524b54cb7cc00d9230046a2905d4f72b920fc
Han Wang <hyw2@cornell.edu>
2021-03-25 16:25:59 -0400

    Update port to support heroku

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.post('/', async (req, res) => {
   res.send('Yoo');
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
