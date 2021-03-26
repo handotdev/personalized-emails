2fd03de272425c7a4f02aec3b8285af02462298d
Han Wang <hyw2@cornell.edu>
2021-03-25 23:47:17 -0400

    Add firebase setup

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const sgMail = require('@sendgrid/mail');
+const admin = require('firebase-admin');
 const articles = require('./articles.json');
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const app = express();
 app.use(bodyParser.json());
+admin.initializeApp({
+  credential: admin.credential.cert(
+    JSON.parse(process.env.FIREBASE_CREDENTIALS)
+  ),
+});
+
+const db = admin.firestore();
 
 app.post('/', async (req, res) => {
   const { email, examples, visuals, logical, storytelling } = req.body;
@@ -27,28 +35,31 @@ app.post('/', async (req, res) => {
   ];
   const topIndex = scoresArr.indexOf(Math.max(...scoresArr));
 
-  const articleGroup = Math.random() > 0.5 ? categoryDict[topIndex] : 'control';
+  const isTest = Math.random() > 0.5;
+  const preference = categoryDict[topIndex];
+  const articleGroup = isTest ? preference : 'control';
   const articleLink = articles[articleGroup];
 
-  console.log(articleGroup);
-  console.log(articleLink);
-
   const msg = {
-    to: email, // Change to your recipient
+    to: email,
     from: 'hyw2@cornell.edu',
     subject: 'Learning styles study: An article tailored for you',
     html: `Hey there,<br/><br/>Thank you for filling out the survey.<br/>Here <a href="${articleLink}">an article about mindfulness and mindlessness</a> tailored to your learning style!<br/><br/>Best,<br/>Han`,
   };
-  sgMail
-    .send(msg)
-    .then(() => {
-      console.log('Email sent');
-    })
-    .catch((error) => {
-      console.error(error);
-    });
 
-  res.send(email);
+  const currentTimestamp = new Date().getTime();
+
+  const docRef = db.collection('signups').doc(currentTimestamp.toString());
+  await docRef.set({
+    email,
+    isTest,
+    preference,
+    timestamp: currentTimestamp,
+  });
+
+  await sgMail.send(msg);
+
+  res.json({ success: true });
 });
 
 const PORT = process.env.PORT || 5000;
